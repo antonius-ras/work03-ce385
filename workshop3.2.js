d40495560686eda64f34bc38863dc823d41d9b10
antonius-ras <antonius.19115@gmail.com>
2026-02-05 16:27:36 +0700

    homework

--- workshop3.2.js
+++ workshop3.2.js
@@ -1,17 +1,36 @@
-function fetchDataWithCallback(callback) {
+const fetchDataWithCallback  = (callback) => {
+    console.log("กำลังดึงข้อมูล callback...");
     setTimeout(() => {
-        const data = "ข้อมูลถูกดึงเรียบร้อย";
-        callback(data);
+        const data = "ข้อมูลที่ไดจาก callback เรียบร้อย";
+        callback(null,data);
     }, 2000);
 }
 
-function fetchDataWithPromise() {
-    return new Promise ((resolve, reject)= {
+fetchDataWithCallback((error, data) => {
+    if(error) {
+        console.log("เกิดข้อผิดพลาด:", error);
+    } else {
+        console.log(data);
+    }
+});
+
+const fetchDataWithPromise = () => {
+    return new Promise ((resolve, reject) => {
+        console.log("กำลังดึงข้อมูลด้วย promise...");
         setTimeout(() => {
         const data = "ข้อมูลที่ไดจาก promise เรียบร้อย"
-        
-        
-    }, 2000);
+        resolve(data);
+    }, 4000);
+    });
+}
+
+fetchDataWithPromise()
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log("เกิดข้อผิดพลาด:", error);
+    });
        
 
     
